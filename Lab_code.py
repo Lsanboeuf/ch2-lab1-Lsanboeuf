a2bc39e96f650fff43284bdd6efa8a7bcf848aa7
Lsanboeuf <54719947+Lsanboeuf@users.noreply.github.com>
2019-09-06 12:49:59 -0400

    Update Lab_code.py

--- Lab_code.py
+++ Lab_code.py
@@ -1,6 +1,18 @@
-# Name: YOUR NAME HERE 
-# Assignment title: ENTER ASSIGNMENT TITLE HERE.
-# Time to complete: ENTER YOUR TIME HERE.
-# Description: ENTER A DESCRIIPTION ABOUT WHAT YOUR CODE IS SUPPOSED TO DO HERE.
+# Laurence SanBoeuf
+# Week 2, Lab 3
+# Time: 1 hr
+# Description: Calculates runoff in gallons with pre-set values. Plot length, plot width, and rainfall in inches. These values can be changed in the code.
 
-Leave the lines of comment code, above, but add your personal details. Then replace these two lines of code with your new code! 
+
+plot_length = 50  #Length of plot area in ft
+plot_width = 20  #Width of plot area in ft
+area = plot_length*plot_width  #Area in ft
+areain = (plot_length*12)* (plot_width*12)  #Area in inches
+rainfall=1  #Amount of recorded rainfall in inches
+rainfall_inches = areain*rainfall  #Volume of rainfall in certain area (inches cubed)
+runoff_gallons = rainfall_inches*0.004329 #Volume of rainfall in area (gallons)
+
+print "Plot_length is:", (plot_length), "ft"
+print "Plot_width is:", (plot_width), "ft"
+print "Rainfall_inches is:",(rainfall_inches), "in^2"
+print "Runoff_gallons is:", (runoff_gallons), "gal"
